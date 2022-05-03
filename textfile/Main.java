a5dce7bac80fb05a3d924a4fdd4b75be49a65116
TAneKAnz <taninwlol001@gmail.com>
2022-05-03 17:41:05 +0700

    texflie

--- textfile/Main.java
+++ textfile/Main.java
@@ -8,13 +8,15 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        demoFileWriter();
         demoFileReader();
+
     }
 
     public static void demoFileWriter() {
         try {
             FileWriter w = new FileWriter("c:\\Users\\tanin\\Test\\textfile\\output.txt");
-            w.write("hello");
+            w.write(unicodeTable('a', 'z'));
             w.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -32,9 +34,19 @@ public class Main {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         
+    }
+
+    public static String unicodeTable(int fromCode, int toCode) {
+        StringBuilder sb = new StringBuilder();
+        //to separate line for every os
+        String ls = System.getProperty("line.separator");
+        for (int i = fromCode; i <= toCode; i++) {
+            sb.append(String.format("X%04X: %-4d: %c", i, i, i));
+            sb.append(ls);
+        }
+        return sb.toString();
     }
 }
